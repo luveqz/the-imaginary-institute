143a19bb7ca4bc8f030d8a17106f8ca1bfec4668
Luis Velásquez <luvejo.dev@gmail.com>
2023-09-21 12:52:03 -0500

    build: setup base font and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,30 @@ module.exports = {
 
   theme: {
     colors: {
+      salmon: {
+        100: '#FFE1DB',
+        400: '#F7876E',
+        500: '#F86653',
+        700: '#D61D04',
+      },
+
+      yellow: {
+        500: '#F5BA25',
+      },
+
       white: '#FFFFFF',
-      black: '#000000',
+      black: '#35312F',
       transparent: 'transparent',
+    },
+
+    fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+
+    extend: {
+      maxWidth: {
+        'content-container': '960px',
+      },
     },
   },
 
